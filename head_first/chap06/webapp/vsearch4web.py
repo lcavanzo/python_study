01cb064c618efdf86e2a1c2d39f9c244b75728a2
lcavanzo <cavanzo1996@gmail.com>
2020-12-15 01:47:37 -0600

    sending data to log files

--- head_first/chap06/webapp/vsearch4web.py
+++ head_first/chap06/webapp/vsearch4web.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from search4letters import search4letters
 
 app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form, req.remote_addr, req.user_agent, res, file=log,
+                sep='|')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -23,6 +24,19 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html', 
         the_title='Welcome to search4letters on the web')
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('view_log.html', the_title='View_Log',
+            the_row_titles=titles,
+            the_data=contents,)
 
 if __name__ == '__main__':
     app.run(debug=True)
