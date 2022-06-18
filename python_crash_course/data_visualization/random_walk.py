ef039a0a58fa2fe8084877dcb22fe9b035652aa8
lcavanzo <cavanzo1996@gmail.com>
2022-06-18 12:17:51 -0500

    data visualization chapter

---
+++ python_crash_course/data_visualization/random_walk.py
@@ -0,0 +1,10 @@
+from random import choice
+
+class RandomWalk:
+    """A Class to generate random walks"""
+    def __init__(self, numb_points=5000):
+        self.pum_points = number_points
+
+    # all walks start at (0, 0)
+    self.x_values = [0]
+    self.y.valuesA = [0]
