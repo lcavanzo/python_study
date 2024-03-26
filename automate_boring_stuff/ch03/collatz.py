34415f309e62f0ffa844f42f9e5de7bcdd87da25
lcavanzo <cavanzo1996@gmail.com>
2024-03-25 22:45:54 -0600

    adding files

--- automate_boring_stuff/ch03/collatz.py
+++ automate_boring_stuff/ch03/collatz.py
@@ -1,16 +1,25 @@
+"""
+Write a function named collatz() that has one parameter named number. If number is even,
+then collatz() should print number // 2 and return this value. If number is odd,
+then collatz() should print and return 3 * number + 1
+"""
+
+
 def collatz(number):
+    """Collatz function"""
     while number != 1:
-        if number % 2 == 0:  # number is even number = number // 2
+        # check if number is even
+        if number % 2 == 0:
             number = number // 2
-            print(f"{number}")
-        else:  # number is odd
+        # check if number is odd
+        else:
             number = 3 * number + 1
-            print(f"{number}")
+        print(f"{number}")
+    return number
 
 
 try:
-    answer = int(input("Enter a number: "))
-except ValueError:
-    print("Integer number is required")
-else:
+    answer = int(input("Enter number: "))
     collatz(answer)
+except ValueError:
+    print("Oops! That was not a valid number. Try again...")
