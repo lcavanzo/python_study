18679688bf2f45b549867e598b8627f774506d7d
lcavanzo <cavanzo1996@gmail.com>
2024-08-05 23:38:56 -0600

    finishing chapter 11

--- automate_boring_stuff/ch11/coin_toss.py
+++ automate_boring_stuff/ch11/coin_toss.py
@@ -1,17 +1,30 @@
 import random
 
-guess = ""
-while guess not in ("heads", "tails"):
-    print("Guess the coin toss! Enter heads or tails: ")
-    guess = input()
 
-toss = random.randint(0, 1)  # 0 is tails, 1 is heads
-if toss == guess:
+def get_guess():
+    while True:
+        guess = input("Guess the coin toss! Enter heads or tails: ").lower()
+        if guess in ("heads", "tails"):
+            return guess
+        print("Invalid input. Please enter 'heads' or 'tails'!!")
+
+
+def coin_toss():
+    if random.randint(0, 1) == 0:
+        return "tails"
+    else:
+        return "heads"
+
+
+guess = get_guess()
+toss = coin_toss()
+
+if guess == toss:
     print("You got it")
 else:
-    print("Nope! Guess it again!")
-    guess = input()
-    if toss == guess:
-        print("You got it!")
+    print("Nope! Guess it again!!")
+    toss = coin_toss()
+    if guess == toss:
+        print("You got it on the second try")
     else:
         print("Nope. You are really bad at this game")
