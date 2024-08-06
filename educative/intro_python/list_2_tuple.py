cead3fcb1844411468fbdd98b0f4d5fb44e92d23
lcavanzo <cavanzo1996@gmail.com>
2024-08-06 15:47:44 -0600

    adding files

---
+++ educative/intro_python/list_2_tuple.py
@@ -0,0 +1,17 @@
+"""
+You are given a list called my_list. Using this list, you must create a tuple called my_tuple.
+The tuple will contain the list’s first element, last element, and the length of the list,
+in that same order.
+
+Sample Input#
+my_list = [34, 82.6, "Darth Vader", 17, "Hannibal"]
+Sample Output
+my_tuple = (34, "Hannibal", 5)
+
+"""
+
+my_list = [34, 82.6, "Darth Vader", 17, "Hannibal"]
+
+# Write your code her
+my_tuple = (my_list[0], my_list[-1], len(my_list))
+print(my_tuple)
