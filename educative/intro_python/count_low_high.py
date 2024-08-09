fdf3ffd6c1cfbc4a278fdb45a001ff1facd29f96
lcavanzo <cavanzo1996@gmail.com>
2024-08-08 22:58:13 -0600

    finishing educative/intro-python

--- educative/intro_python/count_low_high.py
+++ educative/intro_python/count_low_high.py
@@ -12,3 +12,24 @@ num_list = [20, 9, 51, 81, 50, 42, 77]
 Sample Output
 [2, 5] # 2 lows and 5 highs
 """
+
+
+def get_lows_highs(n_list):
+    if len(n_list) == 0:
+        return None
+
+    lows_highs = []
+    lows = 0
+    highs = 0
+    for n in n_list:
+        if n > 50 or n % 3 == 0:
+            highs += 1
+        elif n <= 50 and not n % 3 == 0:
+            lows += 1
+    lows_highs.append(lows)
+    lows_highs.append(highs)
+    return lows_highs
+
+
+num_list = [20, 9, 51, 81, 50, 42, 77]
+print(get_lows_highs(num_list))
