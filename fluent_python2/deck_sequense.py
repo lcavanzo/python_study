4be8acc8e2522a54efc8dc8f7eb00e3aec0a57ad
lcavanzo <cavanzo1996@gmail.com>
2024-08-17 21:51:39 -0600

    adding file

--- fluent_python2/deck_sequense.py
+++ fluent_python2/deck_sequense.py
@@ -25,8 +25,28 @@ suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
 
 def spades_high(card):
-    rank_value = FrenchDeck.ranks.index(card.rank)
+    rank_value = FrenchDeck.ranks.index(card.rank)  # from 0 to 51
     return rank_value * len(suit_values) + suit_values[card.suit]
+    # clubs,2      0   *       4        +   0   = 0
+    # spades,A     12 *         4          + 3  = 51
+    # hearts,8     6  *         4       + 2 = 26
+    # spades,8      6, *        4       + 3 = 27
+    # spades,2      0 *         4       + 3 = 3
+    # clubs,A      12 *         4       + 0 = 48
+    # This doesn't actually happen, but conceptually:
+
+
+# for card in sorted(deck,key=spades_high):
+#   print(card)
+
+# This doesn't actually happen, but conceptually:
+# [
+#     (Card("2", "clubs"), 0),
+#     (Card("2", "diamonds"), 1),
+#     ...(Card("A", "hearts"), 50),
+#     (Card("A", "spades"), 51),
+# ]
+# The sorted() function uses these pairs to sort the cards, then returns just the sorted cards:
 
 
 deck = FrenchDeck()
