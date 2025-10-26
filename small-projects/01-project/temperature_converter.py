5f65333a27a2b3bea99585cf37c71a5b5e2b4876
lcavanzo <cavanzo1996@gmail.com>
2025-10-26 15:45:47 -0600

    update structure

---
+++ small-projects/01-project/temperature_converter.py
@@ -0,0 +1,5 @@
+# temperature_converter.py
+def celsius_to_fahrenheit(celsius):
+    """Converts Celsius to Fahrenheit."""
+    # Implement this correctly after writing tests, or make it return 0 initially.
+    return 0
