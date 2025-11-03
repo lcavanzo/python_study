05dd4d857afad2c198d6829b71132e26cd067afd
lcavanzo <cavanzo1996@gmail.com>
2025-11-03 15:55:44 -0600

    testing

---
+++ educative/fundamental_algorithm_python/merge-2-sorted-list.py
@@ -0,0 +1,36 @@
+# Merge 2 sorted list
+"""
+Merge Two Sorted Lists
+
+Try to solve the Merge Two Sorted Lists problem.
+Statement
+
+Given two integer lists, nums1 and nums2, of size mm and nn, respectively, sorted in nondecreasing order. Merge nums1 and nums2 into a single list sorted in nondecreasing order.
+"""
+
+
+def merged_list(n1, n2):
+    new_list = []
+    p1 = 0
+    p2 = 0
+    p3 = 0
+    while True:
+        print(f"p1:{p1}, p2:{p2}, p3:{p3}")
+        print(f"new_List: {new_list}\n")
+        if n1[p1] < n2[p2]:
+            p3 = p1
+            new_list.append(n1[p3])
+            p1 += 1
+        elif n1[p1] > n2[p2]:
+            p3 = p2
+            new_list.append(n2[p3])
+            p2 += 1
+        else:
+            break
+    return new_list
+
+
+nums1 = [1, 3, 4, 5]
+nums2 = [2, 6, 7, 8]
+
+print(merged_list(nums1, nums2))
