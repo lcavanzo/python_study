fd3bbd1cb593a1b97497186c7f3aaebc16bfde18
luis cavanzo <cavanzo1996@gmail.com>
2025-11-22 23:19:46 -0600

    Exercises/project02 (#17)
    * wip exercise
    
    * wip complete exercise - need improvements
    
    * complete exercise
    
    * complete exercise
    
    * update exercise
    
    * complete exercise
    
    * fix failures
    
    * complete exercise
    
    * update file
    
    * wip exercise
    
    * complete exercise
    
    * complete exercise

--- small-projects/02-project/critical_script.py
+++ small-projects/02-project/critical_script.py
@@ -1,10 +1,15 @@
 import os
+
+
 def do_something_important():
-    print('Performing critical operation...')
+    print("Performing critical operation...")
     # Simulate a critical operation
-    with open('operation_log.txt', 'a') as log_f:
-        log_f.write('Critical operation performed.\n')
-if __name__ == '__main__':
+    with open("operation_log.txt", "a") as log_f:
+        log_f.write("Critical operation performed.\n")
+
+
+if __name__ == "__main__":
     do_something_important()
 
 # Added a comment later
+
