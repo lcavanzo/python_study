dc111e926f2cc1e40063c4e7920cea12a092168c
lcavanzo <cavanzo1996@gmail.com>
2025-12-11 00:53:18 -0600

    wip

--- small-projects/02-project/csv_files/testing/employees_validation.py
+++ small-projects/02-project/csv_files/testing/employees_validation.py
@@ -2,23 +2,39 @@ import csv
 
 
 def validate_csv(filepath):
+    # The Configuration
+    rules = {
+        "Name": {"required": True},
+        "Age": {"required": True},
+    }
+
     with open(filepath, mode="r", newline="", encoding="utf-8") as file:
         reader = csv.reader(file)
+        # 1. Read the header row
         header = next(reader)
-        print(f"Header: {header}")
+
+        # 2. Build the Map, (lookup dictionary) for match and ID with the header
+        # enumerate gives us pairs like: (0, 'Age'), (1, 'Name')
+        header_map = {}
+        for index, column_name in enumerate(header):
+            header_map[column_name] = index
+        print(f"Headers: {header_map}")
+
         for row in reader:
-            is_valid = False
-            name = row[0]
-            age_str = row[1]
+            for column_name, rules_config in rules.items():
+            # row is a list
+            name = row[header_map["Name"]]
+            age_str = row[header_map["Age"]]
+            row_is_valid = True
             if name == "":
                 print("Missing Name")
-                is_valid = False
+                row_is_valid = False
             try:
                 age = int(age_str)
-                is_valid = True
             except ValueError:
+                row_is_valid = False
                 print(f"Error: Invalid Age '{age_str}'")
-            if is_valid:
+            if row_is_valid == True:
                 print(f"Data: {name} {age}")
 
 
