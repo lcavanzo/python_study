03ef54c8bb735c21046538d9bd8dcbeb75b9b4cc
lcavanzo <cavanzo1996@gmail.com>
2025-12-13 19:20:30 -0600

    update code

--- small-projects/02-project/csv_files/testing/employees_validation.py
+++ small-projects/02-project/csv_files/testing/employees_validation.py
@@ -1,34 +1,40 @@
 import csv
 
 
-def validate_csv(filepath):
-    # The Configuration
-    rules = {
-        "Name": {"required": True, "type": "str"},
-        "Age": {"required": True, "type": "int"},
-        "Email": {"required": True},
-    }
+def validate_csv(filepath, expected_headers, validation_rules):
+    errors = []
 
     with open(filepath, mode="r", newline="", encoding="utf-8") as file:
         reader = csv.reader(file)
+
         # 1. Read the header row
-        header = next(reader)
+        try:
+            header = next(reader)
+        except StopIteration:
+            print(f"{filepath} is empty")
+            return
 
         # 2. Build the Map, (lookup dictionary) for match and ID with the header
         # enumerate gives us pairs like: (0, 'Age'), (1, 'Name')
+
         header_map = {}
         for index, column_name in enumerate(header):
             header_map[column_name] = index
-        print(f"Headers: {header_map}")
 
-        for column_name in rules.keys():
+        if list(header_map.keys()) != expected_headers:
+            print(
+                f"Header mismatch: expected_headers={expected_headers} - header={list(header_map.keys())}"
+            )
+            return
+
+        for column_name in validation_rules.keys():
             if column_name not in header_map.keys():
-                print(f"Error: Missing Column: {column_name}")
+                errors.append(f"Error: Missing Column: {column_name}")
                 return
 
         for row in reader:
             row_is_valid = True
-            for column_name, rules_config in rules.items():
+            for column_name, rules_config in validation_rules.items():
                 if column_name in header_map.keys():
                     # 1. Get the Generic Value
                     index = header_map[column_name]
@@ -36,7 +42,7 @@ def validate_csv(filepath):
 
                     # 2. Check Required (Generic)
                     if rules_config["required"] is True and value == "":
-                        print(f"Error: Missing value for {column_name}")
+                        errors.append(f"Error: Missing value for: {column_name}")
                         row_is_valid = False
 
                     # 3. Check Type (Generic)
@@ -45,12 +51,21 @@ def validate_csv(filepath):
                             int(value)
                         except ValueError:
                             row_is_valid = False
-                            print(
+                            errors.append(
                                 f"Error: {column_name} contains invalid number '{value}'"
                             )
             if row_is_valid:
                 print(f"Data: {row}")
+        return errors
+
+
+rules = {
+    "Name": {"required": True, "type": "str"},
+    "Age": {"required": True, "type": "int"},
+}
+
+expected_headers = ["Name", "Age"]
 
 
 csv_file = "employees.csv"
-validate_csv(csv_file)
+validate_csv(csv_file, expected_headers, rules)
