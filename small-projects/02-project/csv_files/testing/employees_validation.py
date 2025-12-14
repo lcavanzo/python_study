b0da0dc2bdc1df92b0c725036f3318e41419d790
lcavanzo <cavanzo1996@gmail.com>
2025-12-13 18:45:37 -0600

    add exercise

--- small-projects/02-project/csv_files/testing/employees_validation.py
+++ small-projects/02-project/csv_files/testing/employees_validation.py
@@ -4,8 +4,9 @@ import csv
 def validate_csv(filepath):
     # The Configuration
     rules = {
-        "Name": {"required": True},
-        "Age": {"required": True},
+        "Name": {"required": True, "type": "str"},
+        "Age": {"required": True, "type": "int"},
+        "Email": {"required": True},
     }
 
     with open(filepath, mode="r", newline="", encoding="utf-8") as file:
@@ -20,22 +21,35 @@ def validate_csv(filepath):
             header_map[column_name] = index
         print(f"Headers: {header_map}")
 
+        for column_name in rules.keys():
+            if column_name not in header_map.keys():
+                print(f"Error: Missing Column: {column_name}")
+                return
+
         for row in reader:
-            for column_name, rules_config in rules.items():
-            # row is a list
-            name = row[header_map["Name"]]
-            age_str = row[header_map["Age"]]
             row_is_valid = True
-            if name == "":
-                print("Missing Name")
-                row_is_valid = False
-            try:
-                age = int(age_str)
-            except ValueError:
-                row_is_valid = False
-                print(f"Error: Invalid Age '{age_str}'")
-            if row_is_valid == True:
-                print(f"Data: {name} {age}")
+            for column_name, rules_config in rules.items():
+                if column_name in header_map.keys():
+                    # 1. Get the Generic Value
+                    index = header_map[column_name]
+                    value = row[index]
+
+                    # 2. Check Required (Generic)
+                    if rules_config["required"] is True and value == "":
+                        print(f"Error: Missing value for {column_name}")
+                        row_is_valid = False
+
+                    # 3. Check Type (Generic)
+                    if rules_config.get("type") == "int":
+                        try:
+                            int(value)
+                        except ValueError:
+                            row_is_valid = False
+                            print(
+                                f"Error: {column_name} contains invalid number '{value}'"
+                            )
+            if row_is_valid:
+                print(f"Data: {row}")
 
 
 csv_file = "employees.csv"
