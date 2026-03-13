c3a59543dcf4d017efba1869b9d30aee9d07799c
lcavanzo <cavanzo1996@gmail.com>
2026-03-12 22:34:01 -0600

    wip

--- small-projects/03-project/encapsulation_abstraction/exercises/shopping_cart.py
+++ small-projects/03-project/encapsulation_abstraction/exercises/shopping_cart.py
@@ -19,27 +19,29 @@ Task:
 
 """
 
+import pytest
+
 
 class ShoppingCart:
     def __init__(self, customer_name):
         self._customer_name = customer_name
-        self._items = {}  # {product_name: quantity}
+        self.__items = {}  # {product_name: quantity}
         self._discount_applied = False
         self._total_price = 0.0
 
     def add_item(self, product_name, price, quantity):
-        if product_name in self._items:
-            self._items[product_name]["quantity"] += quantity
+        if product_name in self.__items:
+            self.__items[product_name]["quantity"] += quantity
         else:
-            self._items[product_name] = {"price": price, "quantity": quantity}
+            self.__items[product_name] = {"price": price, "quantity": quantity}
         self._calculate_total()
 
     def remove_item(self, product_name, quantity):
-        if product_name in self._items:
-            if self._items[product_name]["quantity"] <= quantity:
-                del self._items[product_name]
+        if product_name in self.__items:
+            if self.__items[product_name]["quantity"] <= quantity:
+                del self.__items[product_name]
             else:
-                self._items[product_name]["quantity"] -= quantity
+                self.__items[product_name]["quantity"] -= quantity
             self._calculate_total()
         else:
             print(f"{product_name} not in cart.")
@@ -58,7 +60,7 @@ class ShoppingCart:
 
     def _calculate_total(self):
         current_total = 0.0
-        for product_name, details in self._items.items():
+        for product_name, details in self.__items.items():
             current_total += details["price"] * details["quantity"]
         self._total_price = current_total
         self._discount_applied = False  # Recalculate total, reset discounts status
@@ -66,23 +68,35 @@ class ShoppingCart:
 
     def get_cart_summary(self):
         summary = f"Cart for {self._customer_name}:\n"
-        for product_name, details in self._items.items():
+        for product_name, details in self.__items.items():
             summary += f"- {product_name} (x{details['quantity']}): ${details['price'] * details['quantity']:.2f}\n"
         summary += f"Total: ${self.total_price:.2f}"
         return summary
 
 
 # Testing behaviour
-shopping = ShoppingCart("Luis")
-shopping.add_item(
-    "playstation 5",
-    5000,
-    1,
-)
-shopping.add_item(
-    "pixel phone",
-    3500,
-    2,
-)
-print()
-print(shopping.get_cart_summary())
+# shopping = ShoppingCart("Luis")
+# shopping.add_item(
+#     "playstation 5",
+#     5000,
+#     1,
+# )
+# shopping.add_item(
+#     "pixel phone",
+#     3500,
+#     2,
+# )
+# print()
+# print(shopping.get_cart_summary())
+
+
+# Test file:
+def test_shopping_cart_initialization():
+    sh_cart = ShoppingCart("Luis")
+    assert sh_cart._customer_name == "Luis"
+    # Cannot directly access total_price, so we check through get_salary
+    assert sh_cart.total_price == 0.0
+
+
+if __name__ == "__main__":
+    pytest.main()
